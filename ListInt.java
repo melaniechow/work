fd5f52c596e05b44c26e3fed7e85e63acdb6db82
melaniechow <mchow@stuy.edu>
2016-12-11 10:19:08 -0500

    completed

---
+++ ListInt.java
@@ -0,0 +1,15 @@
+/*
+Team TM: Melanie Chow, Taylor Wong
+APCS pd4
+HW43 -- Array of Titanium
+2016-12-12
+*/
+
+public interface ListInt{
+    void add(int newVal);
+    void add(int index, int newVal);
+    void remove(int index);
+}
+    
+    //add at index, remov e, size
+    
